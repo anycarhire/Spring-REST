88d8011366b4cfddaea2d2eef712f44c4b244d67
Administrator <ylnreddi@gmail.com>
2015-09-05 21:01:14 +0100

    tt

---
+++ src/main/java/spring_rest_webservice/rest/App.java
@@ -0,0 +1,13 @@
+package spring_rest_webservice.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
