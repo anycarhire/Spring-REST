6d94bcd1d616432c391904e59bd37a82bb11892f
Administrator <ylnreddi@gmail.com>
2016-05-23 21:40:37 +0100

    merge commited

--- src/main/java/spring_rest_webservice/rest/App.java
+++ src/main/java/spring_rest_webservice/rest/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+<<<<<<< 398bea444c46efad9676f0bba8dc713a785f4721
         System.out.println( "Hello World!  REDDY" );
+=======
+        System.out.println( "Welcome World!" );
+        System.out.println( "Hello World!" );
+>>>>>>> Update App.java
     }
 }
